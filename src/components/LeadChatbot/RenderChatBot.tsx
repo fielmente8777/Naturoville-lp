108461713ab3fd08da1ee67318caea0e551c077c
abhijeet <abhijeet@eazotel.com>
2025-07-29 22:48:13 +0530

    up

--- src/components/LeadChatbot/RenderChatBot.tsx
+++ src/components/LeadChatbot/RenderChatBot.tsx
@@ -16,7 +16,12 @@ interface EazobotConfig {
   ndid: string;
   interval?: number;
 }
-
+interface Event {
+  data: {
+    type: string;
+    payload: EazobotConfig;
+  };
+}
 interface ChatbotDataFlow {
   domain: string;
   messagesFlow: {
@@ -148,8 +153,9 @@ const RenderChatBot = () => {
     }
   }, [eazbotConfig]);
 
+
   useEffect(() => {
-    const handleMessage = (event: any) => {
+    const handleMessage = (event: Event) => {
       if (event.data?.type === "CHATBOT_INIT") {
         setEazbotConfig(event.data.payload);
       }
