53ffa536586a250c63b1d899d2f623606969dd1f
sumit kumar <sumit@eazotel.com>
2025-12-03 17:18:10 +0530

    refactor: update discount message to reflect current month

--- src/components/pop-up/OfferPopup.tsx
+++ src/components/pop-up/OfferPopup.tsx
@@ -4,15 +4,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { FaGift } from "react-icons/fa6";
-import { IoIosArrowDown } from "react-icons/io";
-import { contact } from "../../../Constent";
 import { MdClose } from "react-icons/md";
+import { contact } from "../../../Constent";
 
 const OfferPopup = () => {
   const [isOpen, setIsOpen] = useState(true);
   const [isMobile, setIsMobile] = useState(false);
 
-  const text = "limited time discount upto 20% off this November.";
+  const currentMonth = new Date().toLocaleString(undefined, { month: "long" });
+
+  const text = `limited time discount upto 20% off this ${currentMonth}.`;
 
   // Auto-close panel after 5 seconds (show tab only)
   useEffect(() => {
