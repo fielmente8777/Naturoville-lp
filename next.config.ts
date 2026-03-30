a876ddfa17881885e7ca90ddd739a77593a2f31d
Personal <sk7627577@gmail.com>
2026-03-30 14:16:50 +0530

    image issue fix

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: "export",
-  // images: {
-  //   unoptimized: true,
-  // },
-  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
+      },
+    ],
+  },
 
+  trailingSlash: true,
 };
 
 export default nextConfig;
